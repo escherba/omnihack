a03bdad0b9f563b989648beb55d32001f5c9083e
Eugene Scherba <escherba@livefyre.com>
2014-07-21 21:04:26 -0700

    add UnionFind structure

---
+++ omnihack/__init__.py
@@ -0,0 +1,2 @@
+from enum import enumerator
+from unionfind import UnionFind
---
+++ tests/test_omni.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python2
+
+import unittest
+from omnihack import enumerator
+
+
+class TestOmni(unittest.TestCase):
+    def test_values(self):
+        i = enumerator()
+        self.assertEqual(i["cat"], 0)
+        self.assertEqual(i["dog"], 1)
+        self.assertEqual(i["cat"], 0)
+        self.assertEqual(len(i), 2)
+
+
+if __name__ == "__main__":
+    unittest.run(verbose=True)
