0572815c981806bd7b74913710439e64115e0846
Eugene Scherba <escherba@livefyre.com>
2014-07-23 09:43:09 -0700

    add docstring

--- pymaptools/utils.py
+++ pymaptools/utils.py
@@ -3,7 +3,16 @@ from copy import deepcopy
 
 
 def deepupdate(d, u):
-    """Recursively update one dict with contents of another"""
+    """
+    Recursively update one dict with contents of another
+
+    :param d: mapping being updated
+    :type d: dict
+    :param u: mapping to update with
+    :type u: collections.Mapping
+    :return: updated mapping
+    :rtype: dict
+    """
     for k, v in u.iteritems():
         d[k] = deepupdate(d.get(k, {}), v) \
             if isinstance(v, collections.Mapping) \
