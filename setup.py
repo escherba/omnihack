0ed4029b99adab0f0f33fdd482224b519f57bcec
Eugene Scherba <escherba@livefyre.com>
2014-08-15 16:12:58 -0700

    version bump

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ dependency_links = dep_links1 + dep_links2
 
 setup(
     name="pymaptools",
-    version="0.0.2",
+    version="0.0.3",
     author="Eugene Scherba",
     author_email="escherba@gmail.com",
     description=("A collection of Python containers for data analysis"),
