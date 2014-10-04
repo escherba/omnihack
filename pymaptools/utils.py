a2efbc25aa4cd5abc0b20c053b36b76320bb71cf
Eugene Scherba <escherba@livefyre.com>
2014-10-04 12:26:54 -0700

    add methods to utils

--- pymaptools/utils.py
+++ pymaptools/utils.py
@@ -1,35 +1,49 @@
 import collections
 from copy import deepcopy
 from pkg_resources import resource_filename
+from contextlib import contextmanager
 
 
-def deepupdate(d, u):
-    """
-    Recursively update one dict with contents of another
+def deepupdate(dest, source):
+    """Recursively update one dict with contents of another
 
-    :param d: mapping being updated
-    :type d: dict
-    :param u: mapping to update with
-    :type u: collections.Mapping
+    :param dest: mapping being updated
+    :type dest: dict
+    :param source: mapping to update with
+    :type source: collections.Mapping
     :return: updated mapping
     :rtype: dict
     """
-    for k, v in u.iteritems():
-        d[k] = deepupdate(d.get(k, {}), v) \
-            if isinstance(v, collections.Mapping) \
-            else v
-    return d
+    for key, value in source.iteritems():
+        dest[key] = deepupdate(dest.get(key, {}), value) \
+            if isinstance(value, collections.Mapping) \
+            else value
+    return dest
 
 
 def override(parent, child):
-    """Inherit child from parent and return a new object"""
+    """Inherit child from parent and return a new object
+    """
     return deepupdate(deepcopy(parent), child)
 
 
-def read_text_file(rel, filename):
-    """Read text file ignoring comments beginning with pound sign"""
-    with open(resource_filename(rel, filename), 'r') as fh:
-        for line in fh:
-            li = line.strip()
-            if not li.startswith('#'):
-                yield li
+def read_text_file(rel, fname):
+    """Read a text resource ignoring comments beginning with pound sign
+    :param rel: path
+    :type rel: str
+    :param fname: path
+    :type fname: str
+    :rtype: generator
+    """
+    with open(resource_filename(rel, fname), 'r') as fhandle:
+        for line in fhandle:
+            stripped = line.strip()
+            if not stripped.startswith('#'):
+                yield stripped
+
+
+@contextmanager
+def empty_context(*args, **kwargs):
+    """Generic empty context wrapper
+    """
+    yield None
