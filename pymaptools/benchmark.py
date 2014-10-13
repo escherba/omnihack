b863e11a1b832b6c4cf5b8b3d6908ec06be013fa
Eugene Scherba <escherba@livefyre.com>
2014-10-13 15:21:45 -0700

    add doctest to PMTimer class

--- pymaptools/benchmark.py
+++ pymaptools/benchmark.py
@@ -2,7 +2,14 @@ import time
 
 
 class PMTimer(object):
-    """Context class for benchmarking"""
+    """Context class for benchmarking
+
+    >>> period = 0.2
+    >>> with PMTimer() as timer:
+    ...     time.sleep(period)
+    >>> timer.wall_interval > period
+    True
+    """
 
     def __init__(self, cpu_clock=time.clock, wall_clock=time.time):
         self.cpu_clock = cpu_clock
