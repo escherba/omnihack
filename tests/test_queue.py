47f2cb8b80398629abcd2281ec9fab2c7870f526
Eugene Scherba <escherba@livefyre.com>
2014-11-14 03:32:46 -0800

    100% coverage on queue module

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -1,18 +1,19 @@
-from pymaptools.queue import OrderedSet
+from pymaptools.queue import OrderedSet, Heap
 import pickle
 import unittest
 
 
 class TestOrderedSet(unittest.TestCase):
-    def test_pickle(self):
+    def test_serialize(self):
         set1 = OrderedSet('abracadabra')
         roundtrip = pickle.loads(pickle.dumps(set1))
         self.assertEqual(roundtrip, set1)
-
-    def test_empty_pickle(self):
         empty_oset = OrderedSet()
+        self.assertEqual(repr(empty_oset), "OrderedSet()")
         empty_roundtrip = pickle.loads(pickle.dumps(empty_oset))
         self.assertEqual(empty_roundtrip, empty_oset)
+        set2 = OrderedSet("abc")
+        self.assertEqual(repr(set2), "OrderedSet(['a', 'b', 'c'])")
 
     def test_order(self):
         set1 = OrderedSet('abracadabra')
@@ -21,9 +22,12 @@ class TestOrderedSet(unittest.TestCase):
         self.assertEqual(list(reversed(set1)), ['d', 'c', 'r', 'b', 'a'])
 
     def test_binary_operations(self):
-        set1 = OrderedSet('abracadabra')
+        text1 = 'abracadabra'
+        set1 = OrderedSet(text1)
         set2 = OrderedSet('simsalabim')
         self.assertNotEqual(set1, set2)
+        self.assertNotEqual(set1, 9)
+        self.assertEqual(set1, set(text1))
 
         self.assertEqual(set1 & set2, OrderedSet(['a', 'b']))
         self.assertEqual(set1 | set2, OrderedSet(['a', 'b', 'r', 'c', 'd', 's', 'i', 'm', 'l']))
@@ -31,6 +35,7 @@ class TestOrderedSet(unittest.TestCase):
 
     def test_slicing(self):
         set1 = OrderedSet('abracadabra')
+        self.assertEqual(set1[0], "a")
         self.assertEqual(set1[:], set1)
         self.assertEqual(set1.copy(), set1)
         self.assertIs(set1[:], set1)
@@ -39,6 +44,9 @@ class TestOrderedSet(unittest.TestCase):
         self.assertEqual(set1[[1, 2]], OrderedSet(['b', 'r']))
         self.assertEqual(set1[1:3], OrderedSet(['b', 'r']))
 
+        with self.assertRaises(TypeError):
+            set1.__getitem__("abc")
+
     def test_maxlen(self):
         setm = OrderedSet(maxlen=3)
         setm.add("a")
@@ -46,3 +54,20 @@ class TestOrderedSet(unittest.TestCase):
         setm.add("c")
         setm.add("d")
         self.assertEqual(setm, OrderedSet(['b', 'c', 'd']))
+
+    def test_discard(self):
+        set1 = OrderedSet("abc")
+        self.assertEqual(len(set1), 3)
+        set1.discard("b")
+        self.assertEqual(len(set1), 2)
+
+
+class TestHeap(unittest.TestCase):
+    def test_basic(self):
+        heap = Heap(2)
+        heap.push("woof", 4)
+        heap.push("meow", 3)
+        heap.push("moo", 10)
+        self.assertEqual(len(heap), 2)
+        self.assertEqual(heap.smallest(), [(4, "woof")])
+        self.assertEqual(heap.largest(), [(10, "moo")])
