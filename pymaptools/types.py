bf4c7e27230b81bbca928de634d2ac1bc4c91e48
Eugene Scherba <escherba@gmail.com>
2015-02-18 14:41:00 -0800

    support setting readonly attrs during object initialization

--- pymaptools/types.py
+++ pymaptools/types.py
@@ -11,15 +11,20 @@ class Struct(object):
     a property it does not know about)
 
     >>> class Duck(Struct):
-    ...     readwrite_attrs = frozenset(["description", "vocalization", "locomotion"])
+    ...     readonly_attrs = frozenset(["description"])
+    ...     readwrite_attrs = frozenset(["vocalization", "locomotion"])
     >>> duck = Duck(description="a medium-size bird")
     >>> duck.vocalization
+    >>> duck.locomotion = "walk, swim, fly"
+    >>> duck.vocalization = "quack"
+    >>> duck.description = "an ostrich"
+    Traceback (most recent call last):
+    ...
+    AttributeError: Attribute 'description' of Duck instance is read-only
     >>> duck.engine_type
     Traceback (most recent call last):
     ...
     AttributeError: 'Duck' object has no attribute 'engine_type'
-    >>> duck.locomotion = "walk, swim, fly"
-    >>> duck.vocalization = "quack"
     >>> duck.laden_speed = "40 mph"
     Traceback (most recent call last):
     ...
@@ -43,27 +48,34 @@ class Struct(object):
     def toDict(self):
         return dict(self.__dict__)
 
-    def _set_attr(self, name, value):
-        """Creates an attribute if one does not exist
+    def __init__(self, **entries):
+        self_dict = self.__dict__
+        readwrite_attrs = self.readwrite_attrs
+        readonly_attrs = self.readonly_attrs
+        for name, value in entries.iteritems():
+            if (name in readwrite_attrs) or (name in readonly_attrs):
+                self_dict[name] = value
+            else:
+                raise AttributeError(
+                    "{} instance has no attribute '{}'".format(
+                        self.__class__.__name__, name))
+
+    def __setattr__(self, name, value):
+        """Set an attribute
+
+        Note that this also sreates an attribute if one does not exist
         but is listed among the readwrite attribute names
         """
         if name in self.readwrite_attrs:
             self.__dict__[name] = value
         elif name in self.readonly_attrs:
             raise AttributeError(
-                "attribute '{}' of instance {} is read-only".format(
+                "Attribute '{}' of {} instance is read-only".format(
                     name, self.__class__.__name__))
         else:
             raise AttributeError(
                 "{} instance has no attribute '{}'".format(
                     self.__class__.__name__, name))
-
-    def __init__(self, **entries):
-        for name, value in entries.iteritems():
-            self._set_attr(name, value)
-
-    def __setattr__(self, name, value):
-        self._set_attr(name, value)
 
     def __getattr__(self, name):
         if name in self.readwrite_attrs or name in self.readonly_attrs:
